76b8193e43167cc248dbb1aa0527cb8f6707377f
awaisajaz1 <awaisajaz1@gamil.com>
2024-01-10 10:36:58 +0500

    Spark connect

--- spark_streams.py
+++ spark_streams.py
@@ -10,9 +10,9 @@ def create_spark_session():
     s_session = None
 
     try:
-        s_session = SparkSession.builder.appName("ApiStreamsFromKafka")\
+        s_session = SparkSession.builder.appName("ApiStreamsFromKafka2")\
             .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0,"
-                    "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1")\
+                    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2")\
             .getOrCreate()
         
         s_session.sparkContext.setLogLevel("ERROR")
@@ -31,6 +31,7 @@ def connect_kafka_cluster(spark_conn):
             .option('subscribe' , 'crowd_realtime_stream') \
             .option('startingOffsets' , 'earliest') \
             .load()
+       
         
         logging.info("dataframe from kafak topic has been created!!!!")
     except Exception as e:
@@ -43,4 +44,12 @@ if __name__ == "__main__":
     
     if spark_connection is not None:
         api_df = connect_kafka_cluster(spark_connection)
-    
+        query  = api_df.writeStream \
+                .format("console") \
+                .outputMode("append") \
+                .trigger(processingTime='10 seconds') \
+                .option("numRows",10)\
+                .option("checkpointLocation", "./checkpoint") \
+                .start()
+
+        query.awaitTermination()
